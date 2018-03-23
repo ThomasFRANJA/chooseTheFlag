35c5d063621b8416268d9eb37664ffd329299f03
ThomasFRANJA <thomas.franja@hetic.net>
2018-03-23 15:09:18 +0100

    version modified

--- app.js
+++ app.js
@@ -1,34 +1,55 @@
 
-var start = document.querySelector('button');
-var pageGame = document.querySelector('#game-start');
-var live = document.querySelectorAll('.lives is-active');
-var flag = document.querySelectorAll('.flag');
-var flagEnd = document.querySelector('.flag4');
-var countrie = document.querySelector('.countrie');
-var time = document.querySelector('.time');
+var startBtn = document.querySelectorAll('.startGame');
+var game     = document.querySelector('#game');
+var live     = document.querySelectorAll('.lives is-active');
+var flag     = document.querySelectorAll('.flag');
+var country  = document.querySelector('#country');
+var timer    = document.querySelector('#timer');
 
+var otherFlags = [];
+var counter    = 0;
+var random     = Math.floor(Math.random() * flags.length);
+var flagToFind = flags[random];
+var seconds    = 1000;
 
-start.addEventListener('click', function(){
+var delay;
+var over = document.getElementById('game-over');
+var restartBtn = document.getElementById('restart');
 
-  var otherFlags = [];
-  var counter = 20;
+function decreaseTimer() {
+  if (timer.textContent === '1'){
+    clearInterval(delay);
+    gameOver();
+  }
+  timer.textContent = timer.textContent - 1;
+}
 
+function startTimer() {
+  delay = setInterval(decreaseTimer, 1000);
+}
 
-   pageGame.classList.toggle('is-open');
-   for (var i = 0; i < 4; i++) {
-     var random = Math.floor(Math.random() * flags.length);
-     var flagToFind = flags[random];
-     flag[i].innerHTML = '<img src ='+flagToFind.src+'></img>';
-     countrie.innerHTML = '<h2>'+flagToFind.name+'</h2>';
+function gameOver() {
+  over.classList.toggle('is-open');
+  timer.textContent = 20;
+}
 
-     // if (flagsToFind === ) {
-     //
-     // }
-   }
+function gameStart(element) {
+  element.parentElement.classList.toggle('is-open');
 
+  for (var i = 0; i < 4; i++) {
+    flag[i].innerHTML = '<img src ='+flagToFind.src+'></img>';
+    country.innerHTML = '<h2>'+flagToFind.name+'</h2>';
+  }
 
-});
+  startTimer();
+}
+
+for (var i = 0; i < startBtn.length; i++) {
+  startBtn[i].addEventListener('click', function(event) {
+    gameStart(event.target);
+  });
+}
 
 //// nom egal au titre ////
-////Front end job web questions (lien mickael)
+//// Front end job web questions (lien mickael)
 //// CSS NEXT : http://cssnext.io/features/
